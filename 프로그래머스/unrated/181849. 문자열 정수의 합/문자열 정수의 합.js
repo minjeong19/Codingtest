820800e3c1615498703d8761817dc96d2855e7cc
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-16 16:01:42 +0900

    [unrated] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,9 +1,8 @@
 function solution(num_str) {
 let result = 0
-const str = num_str.toString()
-for(let i = 0; i<str.length; i++ ){
-  result += Number(str[i])
+for(let i = 0; i<num_str.length; i++ ){
+  result += Number(num_str[i])
 } 
   return result
-
 }
+

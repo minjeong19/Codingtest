5219b412488e95fd3c3e473230e86667808ca588
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-18 20:37:43 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,4 @@
 function solution(array, height) {
-  let result = []
-    for(let i=0; i<array.length; i++){
-      const arr = [...array].sort()
-      if(arr[i]>height){
-        result.push(arr[i])
-      }
-    }return result.length
+ var answer = array.filter(item => item > height);
+    return answer.length;
 }

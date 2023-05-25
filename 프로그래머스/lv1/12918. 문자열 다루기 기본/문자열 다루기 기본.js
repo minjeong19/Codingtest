f680512291e2abe90495d88e7055e1a05db0c805
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-25 17:40:18 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,12 +1,4 @@
 function solution(s) {
-    if(s.length !== 6 && s.length !==4 ){
-      return false
-    } else {
-      const str = s.split('')
-      for(let i =0; i<str.length; i++){
-        if(isNaN(str[i])){
-           return false
-           }
-      }
-    }return true}
-  
+    var answer = true;
+    return answer;
+}

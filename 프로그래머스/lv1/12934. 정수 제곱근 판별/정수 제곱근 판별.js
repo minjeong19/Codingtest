e988e67adc4b98bcb7ad6bb8d6cf134b372541b6
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-25 23:12:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,9 @@
+function solution(n) {
+      let answer = 0;
+    let sqrt = Math.sqrt(n) 
+  if(sqrt % 1 !== 0){
+    answer = -1
+  } else {
+    answer = Math.pow(sqrt+1,2)
+  } return answer
+}

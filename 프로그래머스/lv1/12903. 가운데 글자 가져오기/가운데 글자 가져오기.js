35d8c1aa81390e6d748f3df27a69e0f558450b91
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-26 14:20:01 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+     let answer = 0
+  if(s.length %2 == 0){
+    answer = s[s.length / 2 -1] + s[s.length/2]
+  } else {
+    answer = s[Math.floor(s.length/2)]
+  } return answer
+}

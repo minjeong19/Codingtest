a01d322c56df9fa18e0c863e17d39a914570309b
민정 <55127180+minjeong19@users.noreply.github.com>
2023-05-26 17:52:31 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = '';
+    let arr = s.split(' '); 
+    let str = '' ;
+    for(let i = 0 ;i<arr.length; i++){
+        str = arr[i];
+        for(let j =0;j<str.length;j++){
+            if(j%2 ===0){ 
+                answer += str[j].toUpperCase();
+            }
+            else{
+                answer += str[j].toLowerCase();       
+            }
+        }
+        answer += " ";
+    }
+    answer = answer.substr(0,answer.length-1);
+    return answer;
+}
